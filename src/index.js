c2d95d5c477925eb4fba8a258ac314cb9df1315f
lashon74 <lashonhud@gmail.com>
2020-10-22 21:46:18 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+// Arrow functions makes anonymous function if single input no parenthesis need
+// const newNumbers = numbers.map( x  =>  x * x);
+
+// console.log(newNumbers);
+
+// const newNumbers = numbers.map((x, y) => {
+//   return x * y;
+// });
+
+// console.log(newNumbers);
+
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map((x) => x * 2);
+
+// console.log(newNumbers);
+
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter((num) => num < 10);
+
+// console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// const newNumbers = numbers.reduce(
+//   (accumulator, currentNumber) => accumulator + currentNumber
+// );
+
+// console.log(newNumbers);
+
+////Find - find the first item that matches from an array.
+// const newNumbers = numbers.find((num) => num > 10);
+
+// console.log(newNumbers);
+
+////FindIndex - find the index of the first item that matches.
+// const newNumbers = numbers.findIndex((num) => num > 10);
+
+// console.log(newNumbers);
